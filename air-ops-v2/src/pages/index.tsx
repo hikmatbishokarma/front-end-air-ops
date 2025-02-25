4019fc46445f5a43845ebcf98e29cddd2507090d
yashnoutiyal <yashnoutiyal12@gmail.com>
2025-02-25 21:55:45 +0530

    changes

--- air-ops-v2/src/pages/index.tsx
+++ air-ops-v2/src/pages/index.tsx
@@ -19,7 +19,20 @@ import {
   TextField,
   Button,
   IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@mui/material";
+import Paper from "@mui/material/Paper";
+
+import { BarChart } from '@mui/x-charts/BarChart';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import { dataset, valueFormatter } from './weather';
 import CloseIcon from "@mui/icons-material/Close";
 export default function DashboardPage() {
   const [open, setOpen] = useState(false);
@@ -30,6 +43,61 @@ export default function DashboardPage() {
     console.log(`Reset link sent to: ${email}`);
     setShowEmailSent(true);
   };
+  // chart ˚
+  const chartSetting = {
+    yAxis: [
+      {
+        label: 'rainfall (mm)',
+      },
+    ],
+    width: 500,
+    height: 300,
+    sx: {
+      [`.${axisClasses.left} .${axisClasses.label}`]: {
+        transform: 'translate(-20px, 0)',
+      },
+    },
+  };
+
+
+
+  const [isNewQuote, setIsNewQuote] = useState(false);
+  const [rows, setRows] = useState<any[]>([]);
+
+  const getQuotes = async () => {
+    try {
+      const data = await useGql({
+        query: GET_QUOTES,
+        queryName: "quotes",
+        queryType: "query",
+        variables: {},
+      });
+      setRows(() => {
+        return data.map((quote: any) => {
+          return {
+            id: quote.id,
+            refrenceNo: quote.referenceNumber,
+            status: QuoteStatus[quote.status],
+            requester: quote.requestedBy.name,
+            // representative: quote.representative.name,
+            itinerary: quote.itinerary
+              .map((itinerary: any) => {
+                return `${itinerary.source} - ${itinerary.destination} PAX ${itinerary.paxNumber}`;
+              })
+              .join(", "),
+            createdAt: quote.createdAt,
+            updatedAt: quote.updatedAt,
+          };
+        });
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
 
   return (
     <>
@@ -83,6 +151,73 @@ export default function DashboardPage() {
           </div>
         </div>
       </div>
+
+      <div className="chartsWork">
+        <div className="innerChart_d">
+          <BarChart
+            series={[
+              { data: [35, 44, 24, 34] },
+              { data: [51, 6, 49, 30] },
+              { data: [15, 25, 30, 50] },
+              { data: [60, 50, 15, 25] },
+            ]}
+            height={290}
+            // width={100}
+            xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+            margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+          />
+        </div>
+        <div className="innerChart_d">
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: 10, label: 'series A' },
+                  { id: 1, value: 15, label: 'series B' },
+                  { id: 2, value: 20, label: 'series C' },
+                ],
+              },
+            ]}
+            width={400}
+            height={250}
+          />
+        </div>
+
+      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Requester</TableCell>
+              <TableCell align="right">Itinenary</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                onClick={() => navigate(`/quotes/edit/${row.id}`)}
+              >
+                <TableCell component="th" scope="row">
+                  {row.refrenceNo}
+                </TableCell>
+                <TableCell align="right">{row.status}</TableCell>
+                <TableCell align="right">{row.requester}</TableCell>
+                <TableCell align="right">{row.itinerary}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+
+
+
+      {/* forgot pass work modal */}
+
       <div>
         {/* Button to Open Modal */}
         <Button
