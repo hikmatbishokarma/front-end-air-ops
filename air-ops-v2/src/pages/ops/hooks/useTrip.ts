6248bf1e1f6a13b04c568b00dd7d27b0dab1ffe6
Hikmat BK <hikmat.karma@upgrad.com>
2025-09-06 11:12:00 +0530

    tripdetails

--- air-ops-v2/src/pages/ops/hooks/useTrip.ts
+++ air-ops-v2/src/pages/ops/hooks/useTrip.ts
@@ -2,7 +2,10 @@
 import { useEffect, useState } from "react";
 import useGql from "../../../lib/graphql/gql";
 import { GET_QUOTE_BY_ID } from "../../../lib/graphql/queries/quote";
-import { GET_TRIP_DETAILS } from "../../../lib/graphql/queries/trip-detail";
+import {
+  GET_TRIP_DETAILS,
+  GET_TRIP_DETAILS_BY_ID,
+} from "../../../lib/graphql/queries/trip-detail";
 
 export function useTrip(tripId: string) {
   const [data, setData] = useState<any>(null);
@@ -14,15 +17,23 @@ export function useTrip(tripId: string) {
       try {
         setLoading(true);
 
+        console.log("tripId::::", tripId);
+
+        // const response = await useGql({
+        //   query: GET_TRIP_DETAILS,
+        //   queryName: "tripDetails",
+        //   queryType: "query-with-count",
+        //   variables: { id: tripId },
+        // });
+
         const response = await useGql({
-          query: GET_TRIP_DETAILS,
-          queryName: "tripDetails",
-          queryType: "query-with-count",
+          query: GET_TRIP_DETAILS_BY_ID,
+          queryName: "tripDetail",
+          queryType: "query-without-edge",
           variables: { id: tripId },
         });
 
-        console.log("response:::", response);
-        setData(response.data[0]);
+        setData(response);
       } catch (err) {
         setError(err);
       } finally {
